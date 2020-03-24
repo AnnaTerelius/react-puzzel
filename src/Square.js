0308e496b7c03227cd879c0b8b72dba313297dbd
AnnaTerelius <tereliusanna@hotmail.com>
2020-03-24 23:46:51 +0100

    found indexOf

--- src/Square.js
+++ src/Square.js
@@ -2,10 +2,8 @@ import React, {useState, useEffect} from 'react';
 //let numbers = [0, 1, 2, 7, 4, 5, 6, 3, 8, 9, 10, 11, 12, 13, 14, 15]
 
 export const Square = () => {
-    const [numbers2, setNumbers2] = useState([])
     
-    let numbers = [0, 1, 2, 7, 4, 5, 6, 3, 8, 9, 10, 11, 12, 13, 14, 15]
-    //setNumbers([0, 1, 2, 7, 4, 5, 6, 3, 8, 9, 10, 11, 12, 13, 14, 15]) 
+    let [numbers, setNumbers] = useState([0, 1, 2, 7, 4, 5, 6, 3, 8, 9, 10, 11, 12, 13, 14, 15])
   
     const size = 4;
     const width = 400;
@@ -15,23 +13,46 @@ export const Square = () => {
     const squareWidth = (width / size)
     const squareHeight = (height / size)
     
-    const squareClicked = () =>{
-        alert("click workes")
-        //setNumbers([])
+    const squareClicked = (event) => {
+        //alert("click workes")
+        //if [0]is to the left or right swap places, or if [0] is 4 numbers away swap places
+        console.log(event.target.id)
+        let a = numbers.indexOf(parseInt(event.target.id));
+        let b = numbers.indexOf(0)
+        console.log(a)
+        console.log(b)
+        if ((b - a) === 1 ){
+            console.log('uträkning om 0 är till höger')
+           //numbers[5]= 7 // bytt siffran på plats nr 5 till siffran 7
+           //numbers[b]= event.target.id
+           //numbers[a]= 0
+          // setNumbers(numbers)
+           
+        }else if((a-b) === 1){
+            if(a)
+            console.log('uträkning om 0 är till vänster') 
+        }else if ((a-b) === size){
+            console.log('blir fyra under 0')
+        }else if ((b-a) === size){
+            console.log('blir fyra över 0') 
+        }else {
+            console.log('går inte')
+        }
+             
     }
 
    
-    const shuffleNumbers = () => {
-        var shuffled = numbers.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+    let shuffleNumbers = () => {
+        let shuffled = numbers.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
         return shuffled
        
     }
-    numbers = shuffleNumbers()
+    //numbers = shuffleNumbers()
   
 
-    const clickedBtn = () => {
+    let clickedBtn = () => {
         console.log('clicked btn')
-        numbers = shuffleNumbers()
+        setNumbers(shuffleNumbers)
     }
  
     
@@ -42,7 +63,7 @@ export const Square = () => {
         <button className="btn" onClick={clickedBtn}>shuffle</button>
         <div className="field">
             {numbers.map((number) => (
-                <div className="squares" onClick={squareClicked} style={{backgroundColor: "lightgreen" , width: Math.floor(squareWidth), height: Math.floor(squareHeight), fontSize: Math.floor(height / 6)}}>{number}</div>
+                <div className="squares" id={number} onClick={squareClicked} style={{backgroundColor: "lightgreen" , width: Math.floor(squareWidth), height: Math.floor(squareHeight), fontSize: Math.floor(height / 6)}}>{number}</div>
             ))}
             </div>
             </>
